05c7aeb97a70748074f77f747e49bd3c6e82cdbc
Diego de Mello <diegodemello027@gmail.com>
2025-03-05 20:28:14 -0300

    Criando a camada de UI.

--- src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
+++ src/main/java/br/com/dio/persistence/dao/BoardColumnDAO.java
@@ -6,7 +6,10 @@ import lombok.RequiredArgsConstructor;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+
+import static br.com.dio.persistence.entity.BoardColumnKindEnum.findByName;
 
 @RequiredArgsConstructor
 public class BoardColumnDAO {
@@ -29,6 +32,42 @@ public class BoardColumnDAO {
     }
 
     public List<BoardColumnEntity> findByBoardId(Long id) throws SQLException {
+        List<BoardColumnEntity> entities = new ArrayList<>();
+        var sql = "SELECT id, name, `order` FROM BOARDS_COLUMNS WHERE board_id = ? ORDER BY `order`";
+        try(var statement = connection.prepareStatement(sql)){
+            statement.setLong(1, id);
+            statement.executeQuery();
+            var resultSet = statement.getResultSet();
+            while(resultSet.next()){
+                var entity = new BoardColumnEntity();
+                entity.setId(resultSet.getLong("id"));
+                entity.setName(resultSet.getString("name"));
+                entity.setOrder(resultSet.getInt("order"));
+                entity.setKind(findByName(resultSet.getString("kind")));
+            }
+        }
         return null;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
