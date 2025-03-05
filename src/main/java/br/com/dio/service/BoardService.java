398310e19c9677f850224e83f0b6450a5e0e3eb8
Diego de Mello <diegodemello027@gmail.com>
2025-03-05 18:21:28 -0300

    Criando a camada de acesso a dados.

--- src/main/java/br/com/dio/service/BoardService.java
+++ src/main/java/br/com/dio/service/BoardService.java
@@ -1,5 +1,6 @@
 package br.com.dio.service;
 
+import br.com.dio.persistence.dao.BoardColumnDAO;
 import br.com.dio.persistence.dao.BoardDAO;
 import br.com.dio.persistence.entity.BoardEntity;
 import lombok.AllArgsConstructor;
@@ -13,8 +14,17 @@ public class BoardService {
 
     public BoardEntity insert(final BoardEntity entity) throws SQLException {
         var dao = new BoardDAO(connection);
+        var boardColumnDAO = new BoardColumnDAO(connection);
         try{
             dao.insert(entity);
+            var columns = entity.getBoardColumns().stream().map(c -> {
+               c.setBoard(entity);
+               return c;
+            }).toList();
+            for (var column : columns){
+                boardColumnDAO.insert(column);
+            }
+            connection.commit();
         }catch (SQLException e){
             connection.rollback();
             throw e;
